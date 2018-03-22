32ffbacdac7b0384c25f38b0acfeb76bdd31fe33
Henry Bautista <lord_ergoth@hotmail.com>
2018-03-22 09:45:30 -0500

    Initial Commit

---
+++ test001/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test001
+//
+//  Created by Henry Bautista on 22/03/18.
+//  Copyright © 2018 Henry Bautista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
